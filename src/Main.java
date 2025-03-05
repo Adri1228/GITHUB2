a23a2d52260e833396eb53d5f53ba2ceffb4fc8e
Tutoterrero7 <arcidesmateo@elpuig.xeill.net>
2025-03-05 20:04:49 +0100

    confia2
    Signed-off-by: Tutoterrero7 <arcidesmateo@elpuig.xeill.net>

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,11 @@ public class Main {
 
     }
     private  static void methotd2(){
-
+        System.out.println("que viva la fromula 1!!");
     }
     private  static void methotd3(){
 
     }
     private  static void methotd4(){
-//esto es una prueba para ver como funcionaaaaaa
     }
 }
